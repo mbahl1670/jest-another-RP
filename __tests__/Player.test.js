9dca2051a9fbcb929b1b7fd3757068acebe99589
Michael <mbahl1670@gmail.com>
2021-12-07 18:20:01 -0600

    player constructor

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -7,7 +7,7 @@ test("Creates a player object", () => {
     const player = new Player("Dave");
 
     expect(player.name).toBe("Dave");
-    expect(player.health).toEqual(expect.any(Number))
+    expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
     expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
@@ -30,4 +30,33 @@ test("gets inventory from player or returns false", () => {
     player.inventory = [];
 
     expect(player.getInventory()).toEqual(false);
+});
+
+test("gets player's health value", () => {
+    const player = new Player("Dave");
+    
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test("checks if player is alove or not", () => {
+    const player = new Player("Dave");
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+     
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtracts from player's health", () => {
+    const player = new Player("Dave");
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth -5);
+
+    player.reduceHealth(99999);
+
+    expect(player.health).toBe(0);
 });
